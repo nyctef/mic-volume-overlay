973f4a3c4de0e8b55d4e751da5bdecdaef6baa93
Mark Jordan <Mark.Jordan@red-gate.com>
2023-09-06 11:48:08 +0100

    wip: trying to figure out how to use this com interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use windows::core::{ComInterface, Result};
+use windows::Win32::Media::Audio::Endpoints::IAudioMeterInformation;
+use windows::Win32::System::Com::{CoInitialize, CoUninitialize, CLSCTX_INPROC_SERVER};
+use windows::Win32::{Media::Audio::*, System::Com::CoCreateInstance};
+
+fn main() -> Result<()> {
+    unsafe {
+        CoInitialize(None)?;
+
+        let device_enumerator: IMMDeviceEnumerator =
+            CoCreateInstance(&MMDeviceEnumerator, None, CLSCTX_INPROC_SERVER)?;
+
+        let endpoint = device_enumerator.GetDefaultAudioEndpoint(eCapture, eConsole)?;
+
+        let info: IAudioMeterInformation = endpoint.Activate(CLSCTX_INPROC_SERVER, None)?;
+
+        loop {
+            dbg!(info.GetPeakValue()?);
+            std::thread::sleep(std::time::Duration::from_millis(100));
+        }
+
+        // TODO: do we need to drop anything?
+
+        CoUninitialize();
+        Ok(())
+    }
 }
